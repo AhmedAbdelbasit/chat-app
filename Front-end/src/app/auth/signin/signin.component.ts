9df8c1a31c68de79a68a83db85b8cd0455927a71
AhmedBadee <ahmedbadee17@gmail.com>
2018-07-24 02:56:18 +0200

    injected the AuthService in the SignInComponent to use the signInUser method

--- Front-end/src/app/auth/signin/signin.component.ts
+++ Front-end/src/app/auth/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { User } from '../../../models/user.model';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  onSignIn(user: User) {
+    this.authService.signInUser(user);
+  }
 }
